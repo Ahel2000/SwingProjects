a49168110a5c0f430db64913e4aebf16e98ac361
Ahel Das Chatterjee <aheldc@gmail.com>
2020-10-02 15:20:55 +0530

    Add ChatBotMain

---
+++ ChatBot-SwingAI/ChatBotMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chatbot.swingai;
+
+/**
+ *
+ * @author User
+ */
+import javax.swing.*;
+public class ChatBotMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ChatBot obj=new ChatBot();
+	obj.setTitle("ChatBot");
+	obj.setVisible(true);
+    }
+    
+}
