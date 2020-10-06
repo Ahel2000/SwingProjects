f81d6089997f07a3d5921cd850605333cd10fbb7
Ahel Das Chatterjee <aheldc@gmail.com>
2020-10-06 16:11:06 +0530

    Add files via upload

---
+++ RateATouille/StringConstants.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author User
+ */
+public class StringConstants {
+
+    /**
+     * @param args the command line arguments
+     
+     **/
+    public static final String DB_URL="jdbc:mysql://localhost:3306/rms";
+    public static final String USER="root";
+    public static final String PASS="*******************************";//assign your own password to PASS 
+    
+            
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
